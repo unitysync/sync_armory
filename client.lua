32e3a9421b373a38b9f7f10d0cbd3a1ae54f649b
sync <68277325+unitysync@users.noreply.github.com>
2024-05-20 16:39:18 +1000

    Update client.lua

--- client.lua
+++ client.lua
@@ -133,6 +133,7 @@ end
 
 local function init()
     for type, location in pairs(Config.Locations) do
+        local mainOptions[type] = {}
         local main = mainOptions[type]
         for _, v in pairs(location) do
             -- Register Target for Each Location
@@ -181,7 +182,9 @@ local function init()
     end
     for type, weapons in pairs(Config.Weapons) do
         for _, v in pairs(weapons) do
-            weaponOptions[type][#weaponOptions[type] + 1] = {
+            local weaponOptions[type] = {}
+            local weapons = weaponOptions[type]
+            weapons[#weapons + 1] = {
                 title = v.label,
                 args = { item = v.item, components = v.components },
                 onSelect = getWeapon,
@@ -196,6 +199,7 @@ local function init()
         })
     end
     for type, items in pairs(Config.Items) do
+        local shopOptions[type] = {}
         local shop = shopOptions[type]
         for i = 1, #items do
             shop[#shop + 1] = {
@@ -213,6 +217,7 @@ local function init()
         })
     end
     for type, option in pairs(Config.Armor) do
+        local armorOptions[type] = {}
         local armor = armorOptions[type]
         for _, v in pairs(option) do
             armor[#armor + 1] = {
