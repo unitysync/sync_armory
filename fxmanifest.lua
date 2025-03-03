322bb4a1c6664718ca599f0edc6491d513bbbd92
sync <zxnsync@gmail.com>
2025-03-03 15:29:42 +1000

    chore(manifest): remove oxmysql references

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,22 +7,18 @@ version '1.1.2'
 
 lua54 'yes'
 
-client_script 'client.lua'
-server_scripts {
-    'server.lua',
-    '@oxmysql/lib/MySQL.lua'
-}
-
-
 shared_scripts {
     '@ox_lib/init.lua',
     'config.lua',
     'locales.lua'
 }
 
+client_script 'client.lua'
+server_script 'server.lua'
+
+
 dependencies {
     'ox_lib',
     'ox_inventory',
-    'oxmysql',
     'es_extended'
 }
